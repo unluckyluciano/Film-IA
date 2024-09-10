4ae1d5cde0393f25d1404fa9cc81fdccbea3f8b8
Daria <d.pentangelo1@studenti.unisa.it>
2024-09-10 14:06:33 +0200

    continuo codice

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import numpy as np
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split
+
+# Carichiamo il dataset delle valutazioni e dei film
+ratings = pd.read_csv('ratings.csv')
+movies = pd.read_csv('movies.csv')
+
+# Visualizziamo i primi record
+print(ratings.head())
+print(movies.head())
+# Creiamo la matrice utente-film
+user_movie_matrix = ratings.pivot_table(index='userId', columns='movieId', values='rating')
+user_movie_matrix.fillna(0, inplace=True)
+print(user_movie_matrix.head())
